048f752d77aefaa3bb1f5992cbbc5ee8f3e47dea
Tales Macedo <talesmgbahia@hotmail.com>
2025-08-19 08:07:41 -0300

    feat: adicionar classe Graficos

---
+++ src/graficos.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+class Graficos():
+
+    def plotar_cotovelo(inertias):
+        """Exibie o gráfico do método do cotovelo"""
+
+    def plotar_clusters(df, centroides, colunas, coluna_cluster='Cluster'):
+        """Exibe os clusters em um gráfico 2D"""
+        pass
