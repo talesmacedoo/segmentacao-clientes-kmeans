f7447abe0a79c19f862428130b6194402a53645d
Tales Macedo <talesmgbahia@hotmail.com>
2025-08-27 12:53:41 -0300

    feat(clustering): implementar Clustering com KMeans e padronização de dados

--- src/clustering.py
+++ src/clustering.py
@@ -1,19 +1,27 @@
 from sklearn.cluster import KMeans
+import numpy as np
 
-class AgrupamentoClientes():
-    def __init__(self, num_clusters=3, random_state=42):
-        self.num_clusters = num_clusters
-        self.random_state = random_state
+class Clustering:
+    def __init__(self, df):
+        self.df = df
+        self.X = None
+        self.kmeans = None
+        self.labels = None
 
-    def metodo_cotovelo(self, dados, max_k=10):
-        """Retorna lista de inércias para diferentes k"""
-        pass
+    def preparar_dados(self):
+        # usa exatamente as colunas fornecidas
+        self.X = self.df[['frequencia', 'recencia_exata']].values
 
-    def treinar_prever(self, dados):
-        """Treina o KMeans e retorna os clusters previstos"""
-        pass
+    def metodo_cotovelo(self, max_k=10):
+        sse = []
+        for k in range(1, max_k + 1):
+            km = KMeans(n_clusters=k, random_state=42, n_init='auto')
+            km.fit(self.X)
+            sse.append(km.inertia_)
+        return sse
 
-    def obter_centroides(self):
-        """Retorna os centroides do modelo"""
-        pass
-
+    def rodar_kmeans(self, n_clusters=5):
+        self.kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init='auto')
+        self.kmeans.fit(self.X)
+        self.labels = self.kmeans.labels_
+        return self.kmeans.cluster_centers_
